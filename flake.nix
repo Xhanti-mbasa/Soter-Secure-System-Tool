7f964f08732ff984c46657afd4407b58f6bfc6c2
Xhanti <xhantilomzimbasadaka@gmail.com>
2026-09-21 21:01:19 +0200

    feat(nix): expose Firefox Soterspace app

--- flake.nix
+++ flake.nix
@@ -9,9 +9,14 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages.chromium-pentesting = pkgs.callPackage ./nix/chromium-pentesting.nix {};
+        packages.firefox-pentesting = pkgs.callPackage ./nix/firefox-pentesting.nix {};
         apps.chromium-pentesting = {
           type = "app";
           program = "${self.packages.${system}.chromium-pentesting}/bin/chromium-pentesting";
+        };
+        apps.firefox-pentesting = {
+          type = "app";
+          program = "${self.packages.${system}.firefox-pentesting}/bin/firefox";
         };
       }
     );
